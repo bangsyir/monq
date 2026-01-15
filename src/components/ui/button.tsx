1677b4bde739f74e0d415b1a78d705289194f266
bangsyir <bangsyir@gmail.com>
2026-01-15 23:09:14 +0800

    update components base ui

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,6 +1,8 @@
+"use client";
+
 import { Button as ButtonPrimitive } from "@base-ui/react/button";
-import type { VariantProps } from "class-variance-authority";
-import { cva } from "class-variance-authority";
+import { cva, type VariantProps } from "class-variance-authority";
+
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Separator as SeparatorPrimitive } from "@base-ui/react/separator";
+
 import { cn } from "@/lib/utils";
 
 function Separator({
@@ -13,7 +14,7 @@ function Separator({
 			data-slot="separator"
 			orientation={orientation}
 			className={cn(
-				"shrink-0 bg-border data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-px data-[orientation=vertical]:self-stretch",
+				"data-[orientation=vertical]:center shrink-0 bg-border data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-px",
 				className,
 			)}
 			{...props}
