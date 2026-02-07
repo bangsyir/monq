3a5ce35296b1684b8084e45afd594a116f0ffad2
bangsyir <bangsyir@gmail.com>
2026-02-07 23:38:08 +0800

    add dotenv

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,9 @@
 import { createServerOnlyFn } from "@tanstack/react-start"
 import { drizzle } from "drizzle-orm/postgres-js"
+import dotenv from "dotenv"
 import * as schema from "./schema.ts"
+
+dotenv.config()
 
 const getDatabase = createServerOnlyFn(() => {
   return drizzle(process.env.DATABASE_URL!, { schema })
