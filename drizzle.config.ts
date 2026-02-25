946d1982dda88581b5895a32e6f69ac7d07ceeba
bangsyir <bangsyir@gmail.com>
2026-02-25 23:35:31 +0800

    remove dotenv change to node:process

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,5 @@
-import { config } from "dotenv"
+import * as process from "node:process"
 import { defineConfig } from "drizzle-kit"
-
-config({ path: [".env.local", ".env"] })
 
 export default defineConfig({
   schema: "./src/db/schema.ts",
