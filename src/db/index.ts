701dd3abe651b2fef04c439cdea133b2c15cf6ee
bangsyir <bangsyir@gmail.com>
2026-02-28 18:38:58 +0800

    remove pg and generate new type for wranglers workers

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,11 +1,25 @@
 import * as process from "node:process"
 import { createServerOnlyFn } from "@tanstack/react-start"
 import { drizzle } from "drizzle-orm/neon-http"
-import { neon } from "@neondatabase/serverless"
+import { neon, neonConfig } from "@neondatabase/serverless"
+import ws from "ws"
 import * as schema from "./schema.ts"
 
+// Factory function to create a new database connection for each request
 // This is required for Cloudflare Workers where I/O objects cannot be shared across requests
 export const createDb = createServerOnlyFn(() => {
+  const isLocal = process.env.LOCAL_DEV || false
+  if (isLocal) {
+    // HTTP Mode (recommended for most applications)
+    neonConfig.fetchEndpoint = "http://localhost:5432/sql" // Routes HTTP requests to local proxy
+    neonConfig.poolQueryViaFetch = true // Enables HTTP connection pooling
+
+    // WebSocket Mode (for real-time applications)
+    neonConfig.webSocketConstructor = ws // Enables WebSocket support
+    neonConfig.useSecureWebSocket = false // Local proxy doesn't use SSL
+    neonConfig.wsProxy = () => "localhost:5432" // Routes WebSocket connections to local proxy
+    neonConfig.pipelineConnect = false
+  }
   const sql = neon(process.env.DATABASE_URL)
   return drizzle({ client: sql, schema })
 })
